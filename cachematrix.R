39b1c38baf9395d606e851c0cb6dbedf528e00f5
stinsonk <stinsonk@gmail.com>
2017-04-26 10:41:49 -0400

    Assignment 2 correction

--- cachematrix.R
+++ cachematrix.R
@@ -1,25 +1,34 @@
 ## Functions to create a matrix and cache its inverse.
 
-## This function creates a global object for a given square matrix and its inverse.
+## Function to create a local object for a matrix and its inverse, and a global
+## list of four functions to: Set the Matrix, Get the Matrix data, Set the
+## inverse of the matrix, and get the inverse of the matrix
 
-makecachematrix <- function(x){
-  cachematrix <<- x
-  cachematrixinverse <<- solve(x)
-  invisible(x)
+makecachematrix <- function(matrixdata = matrix()){
+  matrixinverse <- NULL
+  set <- function(matrixinput){
+    matrixdata <<- matrixinput
+    matrixinverse <<- NULL
+  }
+  get <- function() matrixdata
+  setinverse <- function(inverse) matrixinverse <<- inverse
+  getinverse <- function() matrixinverse
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## This function takes a previously defined square matrix, and calculates its inverse
-## if not previously calculated and cached.
 
-cachesolve <- function(x = cachematrix){
-  if(exists("cachematrixinverse") == TRUE){
+## This function checks the local makecachematrix environment for the value of the
+## matrix inverse, calculate it (if not previously done), and output.
+
+cachesolve <- function(x){
+  matrixinverse <- x$getinverse()
+  if(!is.null(matrixinverse)){
     message("Getting cached data.")
-    cachematrixinverse
+    return(matrixinverse)
   }
-  else{
-    message("Calculating matrix inverse.")
-    cachematrixinverse <<- solve(x)
-    cachematrixinverse
-  }
+  matrixdata <- x$get()
+  matrixinverse <- solve(matrixdata)
+  x$setinverse(matrixinverse)
+  matrixinverse
 }
